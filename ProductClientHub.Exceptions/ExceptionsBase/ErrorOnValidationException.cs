1bd6610897f1e6e1e770b0fbcec695c08876a694
Eduol4 <eduardodrevessu@gmail.com>
2025-08-18 18:27:24 -0300

    Adicionado filtro de exceções

--- ProductClientHub.Exceptions/ExceptionsBase/ErrorOnValidationException.cs
+++ ProductClientHub.Exceptions/ExceptionsBase/ErrorOnValidationException.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,5 +16,6 @@ namespace ProductClientHub.Exceptions.ExceptionsBase
         }
 
         public override List<string> GetErrors() => _errors;
+        public override HttpStatusCode GetHttpStatusCode() => HttpStatusCode.BadRequest;
     }
 }
--- ProductClientHub.Exceptions/ExceptionsBase/ProductClientHubException.cs
+++ ProductClientHub.Exceptions/ExceptionsBase/ProductClientHubException.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,5 +14,6 @@ namespace ProductClientHub.Exceptions.ExceptionsBase
         }
 
         public abstract List<string> GetErrors();
+        public abstract HttpStatusCode GetHttpStatusCode();
     }
 }
